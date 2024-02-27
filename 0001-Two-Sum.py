ed153bbc6f8a8b5ee73d940ffbedf271f2ee1796
Joshua Timothy <joshuatimothy.guevara@gmail.com>
2024-02-28 02:09:08 +0800

    Update 0001-Two-Sum.py

--- 0001-Two-Sum.py
+++ 0001-Two-Sum.py
@@ -8,14 +8,18 @@ class Solution:
 
         max_num = max(nums)
 
-        indexes = [None] * (max_num + 1)
+        #indexes = [None] * (max_num + 1)
         # for i in range (max_num):
         #     indexes[i] = None
 
+        # for index_y, y in enumerate(nums):
+        #     indexes[y] = index_y
+
+        indexes = {}
         for index_y, y in enumerate(nums):
             indexes[y] = index_y
 
         for index_x, x in enumerate(nums):
             to_find = target - x
-            if indexes[to_find] is not None and index_x != indexes[to_find]:
+            if to_find in indexes and index_x != indexes[to_find]:
                 return [index_x, indexes[to_find]]
